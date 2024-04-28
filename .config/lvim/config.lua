365667d9e488eaa6b8595ee5273baa4a8409c103
Ramon Blanquer <blanquer.ramon@gmail.com>
2024-04-28 18:44:34 +0200

    [lvim] ChatGPT.nvim working.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -86,7 +86,10 @@ lvim.plugins = {
     'jackMort/ChatGPT.nvim',
     config = function()
       require("chatgpt").setup({
-        api_key_cmd = "echo $OPENAI_API_KEY"
+        api_key_cmd = "echo $OPENAI_API_KEY",
+        openai_params = {
+          model = "gpt-3.5-turbo"
+        }
       })
     end
 
@@ -300,6 +303,25 @@ lvim.builtin.which_key.mappings["t"] = {
   f = { "<cmd>1ToggleTerm<cr>", "Floating terminal" },
   v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
   h = { "<cmd>3ToggleTerm size=10 direction=horizontal<cr>", "Split horizontal" },
+}
+
+-- ChatGPT.nvim
+lvim.builtin.which_key.mappings["z"] = {
+  name = "+ChatGPT",
+  mode = { "n", "v"},
+  c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+  e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+  g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+  t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+  k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+  d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+  a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+  o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+  s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+  f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+  x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+  r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+  l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
 }
 
 -- Define a function to toggle colorcolumn
