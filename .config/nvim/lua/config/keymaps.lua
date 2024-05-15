2f4f0f0eec403a99696ffcb6a1555294fade0a52
Ramon Blanquer <blanquer.ramon@gmail.com>
2024-05-15 11:48:41 +0200

    [nvim] Resize by steps on 1.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Override resize window using <ctrl> arrow keys to be in steps of 1 instead of 2.
+map("n", "<C-Up>", "<cmd>resize +1<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -1<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -1<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +1<cr>", { desc = "Increase Window Width" })
