4b567b38bdd269687c0eee88f0e9cc1872ef0f2d
Ramon Blanquer <blanquer.ramon@gmail.com>
2024-07-05 12:19:41 +0200

    [nvim] Remove config bits removed from upstream

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -12,19 +12,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.shiftwidth = 4
   end,
 })
-
--- -- Cancel properly snippet session upon changing to normal mode.
--- -- https://github.com/L3MON4D3/LuaSnip/issues/258#issuecomment-1429989436
--- -- TODO: Simply disable TAB for cycling snippets completions and use C-n instead, I dislike Tab does so many things.
--- vim.api.nvim_create_autocmd("ModeChanged", {
---   pattern = "*",
---   callback = function()
---     if
---       ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
---       and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
---       and not require("luasnip").session.jump_active
---     then
---       require("luasnip").unlink_current()
---     end
---   end,
--- })
