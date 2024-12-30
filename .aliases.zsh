a2ef4e78d16423d7a03d7bac12c099cb89eb7f41
Ramon Blanquer <blanquer.ramon@gmail.com>
2024-12-30 14:09:51 +0545

    Cleanup aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -3,24 +3,29 @@
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
-
-alias dkcd='docker rmi $(docker images -aq -f "dangling=true")'
-alias dmspindo='docker-machine create --driver digitalocean --digitalocean-access-token `cat $HOME/.tokens/.do`'
 alias ff='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
-alias yt-dl-audio='yt-dlp --ignore-errors --output "%(title)s.%(ext)s" --extract-audio --audio-format mp3'
-alias ox='$HOME/Shadow/Work/ox/bin/ox -c $HOME/.oxrc.tui'
 
+# Docker
+alias dkclean='docker rmi $(docker images -aq -f "dangling=true")'
+alias dkdigioc='docker-machine create --driver digitalocean --digitalocean-access-token `cat $HOME/.tokens/.do`'
+
+# Ox
+alias ox='$HOME/Shadow/Work/ox/bin/ox -c $HOME/.oxrc.tui'
+alias oxguibuildinstall='make app -j8 && sudo rm -rf /Applications/Ox.app && mv target/darwin_app/Ox.app /Applications/'
 alias lastsixcrash='ls -t ~/.six/six_crash_report_* | head -n 1'
 alias visixcrash='vi $(lastsixcrash)'
 alias lastoxcrash='ls -t ~/.ox/ox_crash_report_* | head -n 1'
-alias lastoxextcrash='ls -t ~/.ox/.extension/ox_crash_report_* | head -n 1'
 alias vioxcrash='vi $(lastoxcrash)'
+alias lastoxextcrash='ls -t ~/.ox/.extension/ox_crash_report_* | head -n 1'
+alias vilastoxextcrash='vi $(lastoxextcrash)'
 
 alias oxbuildinstall='make app -j8 && sudo rm -rf /Applications/Ox.app && mv target/darwin_app/Ox.app /Applications/'
 
+# Misc
+alias dlytaudio='yt-dlp --ignore-errors --output "%(title)s.%(ext)s" --extract-audio --audio-format mp3'
+
 # We might need to build some tools against our version of Python, for that we need
 # to install the Python.framework which provides the header
-
 if [[ $(uname) == "Darwin" ]]; then
   # Fixes: pyenv not playing nice with brew "config".
   #
