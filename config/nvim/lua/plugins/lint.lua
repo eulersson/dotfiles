8d49df70dded47d07a2f14d180e72147b8795059
Ramon Blanquer <blanquer.ramon@gmail.com>
2024-12-30 14:11:21 +0545

    Add markdownlint-cli2 configuration

--- config/nvim/lua/plugins/lint.lua
+++ config/nvim/lua/plugins/lint.lua
@@ -7,6 +7,9 @@ return {
           -- Ignore Rule: MD013 Line length
           args = { "--disable", "MD013", "--" },
         },
+        ["markdownlint-cli2"] = {
+          args = { "--config", vim.fn.expand("$HOME") .. "/.markdownlint-cli2.yaml", "--" },
+        },
       },
     },
   },
