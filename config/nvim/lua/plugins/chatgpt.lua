f2c023c05a432b14c4183e35bad500e0fc1e778a
Ramon Blanquer <blanquer.ramon@gmail.com>
2025-03-21 14:53:25 +0545

    Remove GPG from ChatGPT.nvim

--- config/nvim/lua/plugins/chatgpt.lua
+++ config/nvim/lua/plugins/chatgpt.lua
@@ -1,17 +1,13 @@
 return {
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup({
-        api_key_cmd = "gpg --decrypt " .. vim.fn.expand("$HOME") .. "/openai-api-token.txt.gpg",
-      })
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim", -- optional
-      "nvim-telescope/telescope.nvim",
-    },
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup()
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim", -- optional
+    "nvim-telescope/telescope.nvim",
   },
 }
