92550a2e2f448ac3e74994a18051e4fbe5ed2b95
Ramon Blanquer <blanquer.ramon@gmail.com>
2025-03-21 14:54:38 +0545

    Remove dead code

--- config/nvim/lua/plugins/lint.lua
+++ config/nvim/lua/plugins/lint.lua
@@ -3,10 +3,6 @@ return {
     "mfussenegger/nvim-lint",
     opts = {
       linters = {
-        markdownlint = {
-          -- Ignore Rule: MD013 Line length
-          args = { "--disable", "MD013", "--" },
-        },
         ["markdownlint-cli2"] = {
           args = { "--config", vim.fn.expand("$HOME") .. "/.markdownlint-cli2.yaml", "--" },
         },
