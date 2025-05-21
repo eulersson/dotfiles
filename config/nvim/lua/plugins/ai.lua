986f731c02dc72eaa6c6deb3df8b6e8aad25f50b
Ramon Blanquer <blanquer.ramon@gmail.com>
2025-05-21 17:08:25 +0200

    Add code companion

---
+++ config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+}
