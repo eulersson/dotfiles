773a6ca961690deb41f3ca5bdc53f976274a8a29
Ramon Blanquer <blanquer.ramon@gmail.com>
2025-06-27 13:33:12 +0200

    fix(lazyvim): git status to now show difft result

---
+++ config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,16 @@
+return {
+
+  {
+    "fzf-lua",
+    opts = {
+      previewers = {
+        git_diff = {
+          -- ensure the default tool is used instead of the customized one (difft)
+          cmd_deleted = "git diff --no-ext-diff --color HEAD --",
+          cmd_modified = "git diff --no-ext-diff --color HEAD",
+          cmd_untracked = "git diff --no-ext-diff --color --no-index /dev/null",
+        },
+      },
+    },
+  },
+}
