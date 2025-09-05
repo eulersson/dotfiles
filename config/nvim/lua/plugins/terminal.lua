2ce94819fd2cdec89c87b7e6d2e2e82cb08aa393
Ramon Blanquer <blanquer.ramon@gmail.com>
2025-09-05 21:06:06 +0200

    chore(lazyvim): update snacks terminal configuration
    So that when focusing the terminal it uses NORMAL mode instead of
    TERMINAL mode and that it auto closes when exiting.

---
+++ config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      auto_insert = false, -- boolean start insert mode when entering the terminal buffer
+      auto_close = true, -- close the terminal buffer when the process exits
+    },
+  },
+}
