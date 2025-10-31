09bda4f4d79da06c3330992d213c7fd63f9b371b
Ramon Blanquer <blanquer.ramon@gmail.com>
2025-10-31 11:03:29 +0100

    chore(lazyvim): always load diffview.nvim

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
   {
     "sindrets/diffview.nvim",
+    lazy = false,
     keys = {
       { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
       { "<leader>gi", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview File History" },
