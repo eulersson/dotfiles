d775d5d9385d36d2022b0944f2c49f5076e41067
Ramon Blanquer <blanquer.ramon@gmail.com>
2026-01-25 13:31:38 +0100

    chore(lazyvim): fix history for telescope-live-grep-args.actions

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -7,34 +7,12 @@ return {
     dependencies = {
       {
         "nvim-telescope/telescope-live-grep-args.nvim",
+        {
+          "nvim-telescope/telescope-fzf-native.nvim",
+          build = "make",
+        },
       },
     },
-    opts = function(_, opts)
-      local lga_actions = require("telescope-live-grep-args.actions")
-
-      opts.fzf_opts = {
-        ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-history",
-      }
-
-      opts.extensions = {
-        live_grep_args = {
-          auto_quoting = true, -- enable/disable auto-quoting
-          -- define mappings, e.g.
-          mappings = { -- extend mappings
-            i = {
-              ["<C-k>"] = lga_actions.quote_prompt(),
-              ["<C-o>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-              ["<C-n>"] = lga_actions.cycle_history_next,
-              ["<C-p>"] = lga_actions.cycle_history_prev,
-            },
-          },
-          -- ... also accepts theme settings, for example:
-          -- theme = "dropdown", -- use dropdown theme
-          -- theme = { }, -- use own theme spec
-          -- layout_config = { mirror=true }, -- mirror preview pane
-        },
-      }
-    end,
     keys = {
       {
         "<leader>/",
@@ -42,10 +20,38 @@ return {
         desc = "Grep Live Args (root dir)",
       },
     },
-    config = function(_, opts)
+    config = function()
       local telescope = require("telescope")
-      telescope.setup(opts)
+      local actions = require("telescope.actions")
+      local lga_actions = require("telescope-live-grep-args.actions")
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<CR>"] = actions.select_default + actions.center,
+            },
+            n = {
+              ["<CR>"] = actions.select_default + actions.center,
+            },
+          },
+        },
+        extensions = {
+          live_grep_args = {
+            mappings = {
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                ["<C-n>"] = actions.cycle_history_next,
+                ["<C-p>"] = actions.cycle_history_prev,
+              },
+            },
+          },
+        },
+      })
+
       telescope.load_extension("live_grep_args")
+      telescope.load_extension("fzf")
     end,
   },
 }
