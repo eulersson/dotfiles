103c548e7bd8d981567fd4f05eed72ed53caa34a
Ramon Blanquer <blanquer.ramon@gmail.com>
2026-03-31 18:11:06 +0200

    fix(lazyvim): use zen from snacks so settings are respected

--- config/nvim/lua/plugins/zen_mode.lua
+++ config/nvim/lua/plugins/zen_mode.lua
@@ -1,33 +1,25 @@
 return {
+  -- configure Snacks zen mode (triggered by <leader>uz)
   {
-    "folke/zen-mode.nvim",
-    dependencies = { "folke/twilight.nvim" },
-    event = "VeryLazy",
+    "folke/snacks.nvim",
     opts = {
-      window = {
-        backdrop = 0.8,
-        width = 90,
-        height = 0.6,
-        options = {
-          number = false, -- disable number column
-          relativenumber = false, -- disable relative numbers
-          cursorline = false, -- disable cursorline
-          cursorcolumn = false, -- disable cursor column
-          foldcolumn = "0", -- disable fold column
-          list = false, -- disable whitespace characters
+      zen = {
+        toggles = {
+          dim = true,
+          git_signs = false,
+          mini_diff_signs = false,
         },
-      },
-      plugins = {
-        options = {
-          enabled = true,
-          ruler = true, -- disables the ruler text in the cmd line area
-          showcmd = true, -- disables the command in the last line of the screen
-          laststatus = 0, -- turn off the statusline in zen mode
-        },
-        twilight = { enabled = true },
-        kitty = {
-          enabled = true,
-          font = "+7",
+        win = {
+          style = "zen",
+          width = 120,
+          wo = {
+            number = false,
+            relativenumber = false,
+            cursorline = false,
+            cursorcolumn = false,
+            foldcolumn = "0",
+            list = false,
+          },
         },
       },
     },
