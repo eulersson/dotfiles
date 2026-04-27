78b520ffe161ee49ce60d0678346bc7a93817a09
Ramon Blanquer <blanquer.ramon@gmail.com>
2026-04-27 19:54:12 +0200

    fix(theme): re-sync nvim appearance on FocusGained

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -10,6 +10,13 @@ return {
     config = function()
       -- dark-notify changes the background option and tokyonight automatically reacts
       require("dark_notify").run()
+      -- Re-sync on focus regain (e.g. tmux session switch) in case the
+      -- subprocess missed an event. Requires `focus-events on` in tmux.
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          require("dark_notify").update()
+        end,
+      })
     end,
   },
 }
